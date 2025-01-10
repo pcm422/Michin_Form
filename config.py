2db2203ffcd1590a45cd873a54127a4dc8875076
pcm422 <pcm0422@gmail.com>
2025-01-10 13:28:49 +0900

    ✨ 기본 세팅
    파일 생성 및 일부 파일(db를 포함) 수정

---
+++ config.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysqlm/root:1234@127.0.0.1/mini_michin_form"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 10
+    SQLALCHEMY_POOL_TIMEOUT = 5
+    SQLALCHEMY_POOL_RECYCLE = 1800
+    SQLALCHEMY_MAX_OVERFLOW = 5
+    SQLALCHEMY_ECHO = False
+    reload = True
